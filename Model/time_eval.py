10f545a308011b06ba2a5ab6061ed4eb6dc97719
kleeritter <steding@proton.me>
2023-08-10 11:42:37 +0200

    Test

--- Model/time_eval.py
+++ Model/time_eval.py
@@ -55,8 +55,8 @@ def train(forecast_var,forecast_horizont,window_size,transfer_learning=False,cor
 if __name__ == '__main__':
     forecast_vars=["rain"]#[ "temp","press_sl", "humid", "diffuscmp11", "globalrcmp11", "gust_10", "gust_50",     "rain", "wind_10", "wind_50","wind_dir_50_sin", "wind_dir_50_cos"]
     for forecast_var in forecast_vars:
-        for window_size in tqdm([24]):#,8*7*24,4*7*24,2*7*24,7*24,6*24,4*24,3*24,2*24,24,12,6,3]):
-            for forecast_horizont in [24]:#2,4,6,12,15,18,24,32,48,60,72,84,96,192]:
+        for window_size in tqdm([2*7*24,7*24,6*24,5*24,4*24,3*24,2*24,24,12,6,3]):#,8*7*24,4*7*24,2*7*24,7*24,6*24,4*24,3*24,2*24,24,12,6,3]):
+            for forecast_horizont in [2,4,6,12,15,18,24,32,48,60,72,84,96,192]:
                 #if window_size!=5*24 :
                    # train(forecast_var,forecast_horizont,window_size,transfer_learning=True)
                 #else:
@@ -65,5 +65,5 @@ if __name__ == '__main__':
                 model_path='timetest/lstm_multi/models/best_model_state_'+forecast_var+'_'+str(window_size)+'_'+str(forecast_horizont)+'.pt'
                 params_folder='opti/output/lstm_multi/best_params_lstm_multi_'+forecast_var+'.yaml'
                 varlist = [forecast_var]
-                output_file='timetest/lstm_multi/output/all/timetest_lstm_multi'+forecast_var+'_'+str(window_size)+'_'+str(forecast_horizont)+'.nc'
+                output_file='timetest/lstm_multi/output/'+forecast_var+'/timetest_lstm_multi'+forecast_var+'_'+str(window_size)+'_'+str(forecast_horizont)+'.nc'
                 forecast_lstm_multi(var_list=varlist, model_folder=model_path, params_folder=params_folder,forecast_horizont=forecast_horizont,window_size=window_size,output_file=output_file,corvars=[ "temp","press_sl", "humid", "diffuscmp11", "globalrcmp11", "gust_10", "gust_50",     "rain", "wind_10", "wind_50","wind_dir_50_sin", "wind_dir_50_cos","taupunkt","Taupunkt3h","press3h","rainsum3h","temp3h","gradwind","rain_event"])
