67f59b0c0497f769a6d522d96221bdc4c4cb359f
Alex Steding <steding@proton.me>
2023-11-06 11:55:16 +0100

    test

--- converting/netcdf_convert.py
+++ converting/netcdf_convert.py
@@ -10,9 +10,18 @@ def convert_months(path="/data/datenarchiv/imuk/", year="2022", month="1"):
     
     return
 
-def convert_days(path="/data/datenarchiv/imuk/", year="2022", month="1", day="11"):
-    herrenhausen_data= pd.read_csv(path+"herrenhausen/"+year+"/hh"+year+month.zfill(2)+day.zfill(2)+".csv", delimiter=";")
-    print(herrenhausen_data.head())
+def convert_days(path="/data/datenarchiv/imuk/", year="2022", month="1", day="11", location="Herrenhausen"):
+   
+    if location== "Herrenhausen":
+        herrenhausen_data= pd.read_csv(path+"herrenhausen/"+year+"/hh"+year+month.zfill(2)+day.zfill(2)+".csv", delimiter=";")
+        print(herrenhausen_data.head())
+
+        dach_data = pd.read_csv(path+"dach/"+year+"/kt"+year+month.zfill(2)+day.zfill(2)+".csv", delimiter=";")
+        print(herrenhausen_data.head())
+    else:
+        ruthe_data= "ruthe"
+
+
     return
 
 
