d8d50ba8eda009ab4bb45aa0d9b60caec029aab0
Alex Steding <steding@proton.me>
2023-11-06 11:44:49 +0100

    test

---
+++ converting/netcdf_convert.py
@@ -0,0 +1,19 @@
+import xarray as xr
+import pandas as pd
+
+
+def convert_years(path="/data/datenarchiv/imuk/", year="2022"):
+
+    return
+
+def convert_months(path="/data/datenarchiv/imuk/", year="2022", month="1"):
+    
+    return
+
+def convert_days(path="/data/datenarchiv/imuk/", year="2022", month="1", day="11"):
+    herrenhausen_data= pd.open_csv(path+"hh"+year+month.zfill(2)+day.zfill(2)+".csv")
+    print(herrenhausen_data.head())
+    return
+
+
+convert_days()
