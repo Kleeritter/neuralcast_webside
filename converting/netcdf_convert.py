280c943486f4c1cfcbcf4b74899e3f5f90cc564a
Alex Steding <steding@proton.me>
2023-11-07 11:05:19 +0100

    test

--- converting/netcdf_convert.py
+++ converting/netcdf_convert.py
@@ -21,6 +21,32 @@ def attribute_transfer(xarray_dataset, location="Herrenhausen"):
                 xarray_dataset[var].attrs[key] = value
     return xarray_dataset
 
+
+def herrenhausen_tools(herrenhausen_data):
+    herrenhausen_data.rename(columns={herrenhausen_data.columns[0]: "time",herrenhausen_data.columns[7]: "Wind_Speed",herrenhausen_data.columns[9]: "Gust_Speed"}, inplace=True)
+    herrenhausen_data["time"] = pd.to_datetime(herrenhausen_data["time"], format="%d.%m.%Y %H:%M:%S")#'%Y-%m-%d %H:%M:%S')
+    herrenhausen_data.set_index('time', inplace=True)
+    new_column_names = {col: f'herrenhausen_{col.lstrip()}' for col in herrenhausen_data.columns}
+    herrenhausen_data.rename(columns=new_column_names, inplace=True)
+    return herrenhausen_data
+
+def dach_tools(dach_data):
+    dach_data.rename(columns={dach_data.columns[0]: "time"}, inplace=True)
+    dach_data["time"] = pd.to_datetime(dach_data["time"], format="%d.%m.%Y %H:%M:%S")#'%Y-%m-%d %H:%M:%S')
+    dach_data.set_index('time', inplace=True)
+    new_column_names = {col: f'dach_{col.lstrip()}' for col in dach_data.columns}
+    dach_data.rename(columns=new_column_names, inplace=True)
+    return dach_data
+
+
+def sonic_tools(sonic_data):
+    sonic_data.rename(columns={sonic_data.columns[0]: "time",sonic_data.columns[2]: "Wind_Speed",sonic_data.columns[3]: "Wind_Dir",sonic_data.columns[4]: "Gust_Speed"}, inplace=True)
+    sonic_data["time"] = pd.to_datetime(sonic_data["time"], format="%d.%m.%Y %H:%M")#'%Y-%m-%d %H:%M:%S')
+    sonic_data.set_index('time', inplace=True)
+    new_column_names = {col: f'sonic_{col.lstrip()}' for col in sonic_data.columns}
+    sonic_data.rename(columns=new_column_names, inplace=True)
+    return sonic_data
+
 def convert_years(path="/data/datenarchiv/imuk/", year="2022"):
 
     return
@@ -33,27 +59,15 @@ def convert_days(path="/data/datenarchiv/imuk/", year="2022", month="1", day="11
    
     if location== "Herrenhausen":
         herrenhausen_data= pd.read_csv(path+"herrenhausen/"+year+"/hh"+year+month.zfill(2)+day.zfill(2)+".csv", delimiter=";")
-        herrenhausen_data.rename(columns={herrenhausen_data.columns[0]: "time",herrenhausen_data.columns[7]: "Wind_Speed",herrenhausen_data.columns[9]: "Gust_Speed"}, inplace=True)
-        herrenhausen_data["time"] = pd.to_datetime(herrenhausen_data["time"], format="%d.%m.%Y %H:%M:%S")#'%Y-%m-%d %H:%M:%S')
-        herrenhausen_data.set_index('time', inplace=True)
-        new_column_names = {col: f'herrenhausen_{col.lstrip()}' for col in herrenhausen_data.columns}
-        herrenhausen_data.rename(columns=new_column_names, inplace=True)
-        print(herrenhausen_data.head())
+        herrenhausen_data=herrenhausen_tools(herrenhausen_data)
+        #print(herrenhausen_data.head())
 
         dach_data = pd.read_csv(path+"dach/"+year+"/kt"+year+month.zfill(2)+day.zfill(2)+".csv", delimiter=";")
-        dach_data.rename(columns={dach_data.columns[0]: "time"}, inplace=True)
-        dach_data["time"] = pd.to_datetime(dach_data["time"], format="%d.%m.%Y %H:%M:%S")#'%Y-%m-%d %H:%M:%S')
-        dach_data.set_index('time', inplace=True)
-        new_column_names = {col: f'dach_{col.lstrip()}' for col in dach_data.columns}
-        dach_data.rename(columns=new_column_names, inplace=True)
+        dach_data = dach_tools(dach_data)
         print(dach_data.head())
 
         sonic_data = pd.read_csv(path+"sonic/"+year+"/sonic"+year+month.zfill(2)+day.zfill(2)+".txt", delimiter=";")
-        sonic_data.rename(columns={sonic_data.columns[0]: "time",sonic_data.columns[2]: "Wind_Speed",sonic_data.columns[3]: "Wind_Dir",sonic_data.columns[4]: "Gust_Speed"}, inplace=True)
-        sonic_data["time"] = pd.to_datetime(sonic_data["time"], format="%d.%m.%Y %H:%M")#'%Y-%m-%d %H:%M:%S')
-        sonic_data.set_index('time', inplace=True)
-        new_column_names = {col: f'sonic_{col.lstrip()}' for col in sonic_data.columns}
-        sonic_data.rename(columns=new_column_names, inplace=True)
+        sonic_data =sonic_tools(sonic_data)
         print(sonic_data.head())
 
         merged_data = pd.concat([herrenhausen_data, dach_data, sonic_data], axis=1)
