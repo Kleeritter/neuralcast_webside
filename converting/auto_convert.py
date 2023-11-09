56234e2941cfebf332bc615bb08cad7bebc1b078
Alex Steding <steding@proton.me>
2023-11-09 13:21:11 +0100

    test

--- converting/auto_convert.py
+++ converting/auto_convert.py
@@ -1,32 +1,39 @@
 from netcdf_convert import *
 import argparse
+
+from datetime import date
 def main():
     ##Parsing Variable Values
     ##Parsing Variable Values
     parser = argparse.ArgumentParser()
+    #parser.add_argument("")
     parser.add_argument('inputpath')
-    parser.add_argument('year')  #
-    parser.add_argument('full')
-    parser.add_argument('startday')
-    parser.add_argument('endday')
-    parser.add_argument('location')
-    parser.add_argument('outputpath')
+    #parser.add_argument('year')  #
+    #parser.add_argument('full')
+    #parser.add_argument('startday')
+    #parser.add_argument('endday')
+    #parser.add_argument('location')
+    parser.add_argument('outputpath_herrenhausen')
+    parser.add_argument('outputpath_ruthe')
 
     args = parser.parse_args()  # gv[480#210    #480
-    year = int(args.year)
-    if args.full == "True":
-        full= True
-    else:
-        full=False
-    filename = args.outputpath
-    path = args.inputpath
+    #year = int(args.year)
+    #if args.full == "True":
+     #   full= True
+    #else:
+     #   full=False
 
-    startday = args.startday
-    endday = args.endday
-    location=args.location
+    path = args.inputpath
+    today = date.today()
+    startday= today.strftime('%Y') +"-01-01"
+    endday = today.strftime('%Y-%m-%d')
+    outputpath_herrenhausen = args.outputpath_herrenhausen + startday+"-"+endday+"_herrenhausen.nc"
+    outputpath_ruthe = args.outputpath_ruthe+ startday+"-"+endday+"_ruthe.nc"
+    #location=args.location
     #os.chdir(dir_Produkt)
     #convert_years(path="/data/datenarchiv/imuk/", year=2022,full=True, startday="2022-01-01",endday="2022-03-01", location="Herrenhausen", filename="test_year.nc")
-    convert_years(path=path, year=year,full=full, startday=startday,endday=endday, location=location, filename=filename)
+    convert_years(path=path, year=year,full=False, startday=startday,endday=endday, location="Herrenhausen", filename=outputpath_herrenhausen)
+    convert_years(path=path, year=year,full=False, startday=startday,endday=endday, location="Ruthe", filename=outputpath_ruthe)
 
 
 if __name__ == "__main__":
--- converting/manual_convert.sh
+++ converting/manual_convert.sh
@@ -3,13 +3,17 @@ conda activate stadtwetter
 #export PYTHONPATH=$PYTHONPATH:/localdata/weathermaps/imuk
 
 path_input=/data/datenarchiv/imuk/
-path_output_herrenhausen=""
-path_output_ruthe=""
+year=2022
+full="True"
+startday="2022-01-01"
+endday="2022-03-01"
+location="Herrenhausen"
+filename="test_year.nc"
 #path_output=/localdata/weathermaps/webside/gross
 
 
 
 #convert_years(path="/data/datenarchiv/imuk/", year=2022,full=True, startday="2022-01-01",endday="2022-03-01", location="Herrenhausen", filename="test_year.nc")
 
-python /home/stadtwetter/neuralcast_webside/converting/auto_convert.py $path_input $path_output_herrenhausen $path_output_ruthe
+python /home/stadtwetter/neuralcast_webside/converting/manual_convert.py $path_input $year $full $startday $endday $location $filename
 echo "Finished"
