3a0118ea0496d6584b0aceb3586d6cd68da4409f
Alex Steding <steding@proton.me>
2023-12-15 19:30:35 +0100

    fix

--- auto_forecast.py
+++ auto_forecast.py
@@ -26,7 +26,7 @@ def main():
     print("start Multi")
     dataset="test_data/latest_herrenhausen_normal_imuknet1.nc"
     outputfile = output+"/forecast_test.nc"
-    neural_forecast(dataset=dataset,outputfile=outputfile, time_start= time_start)
+    neural_forecast_multi(dataset=dataset,outputfile=outputfile, time_start= time_start)
     #### Multi Forecast Ende ###
     
     ### Imuknet1 Forecast Ende ###
