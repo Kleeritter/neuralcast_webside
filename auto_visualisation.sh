af50a58c95d324e3e78dea97b6cca24c9793f391
Alex Steding <steding@proton.me>
2024-01-17 17:03:08 +0100

    niere

--- auto_visualisation.sh
+++ auto_visualisation.sh
@@ -4,8 +4,8 @@ source /home/stadtwetter/.bashrc
 #export PYTHONPATH=$PYTHONPATH:/localdata/weathermaps/imuk
 
 path_input_measured=/data/stadtwetter/netcdf_daten/latest_herrenhausen_res_imuknet1.nc
-path_input_forecast_single=/data/stadtwetter/Vorhersage/forecast_multi.nc
-path_input_forecast_multi=/data/stadtwetter/Vorhersage/forecast_test.nc
+path_input_forecast_single=/data/stadtwetter/Vorhersage/forecast_test.nc
+path_input_forecast_multi=/data/stadtwetter/Vorhersage/forecast_test_single.nc
 path_output=/data/stadtwetter/Vorhersage/Grafiken
 
 /home/stadtwetter/miniforge3/envs/Stadtwetter/bin/python3 /home/stadtwetter/neuralcast_webside/auto_visualisation.py $path_input_measured $path_input_forecast_single $path_input_forecast_multi $path_output
