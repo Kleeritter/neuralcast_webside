55d8d930eb8a1ea6a93b247e6bec6e855a649ebd
Alex Steding <steding@proton.me>
2024-02-29 15:59:58 +0100

    e

--- auto_script.sh
+++ auto_script.sh
@@ -37,7 +37,7 @@ path_input_measured="/data/stadtwetter/netcdf_daten/latest_herrenhausen_res_imuk
 path_input_forecast_single="/data/stadtwetter/Vorhersage/forecast_test_single_multiday.nc"
 path_input_forecast_multi="/data/stadtwetter/Vorhersage/forecast_test.nc"
 
-path_output="/data/stadtwetter/Vorhersage/grafiken/test/"
+path_output="/data/stadtwetter/Vorhersage/Grafiken/test/"
 debug=1  # true
 
 "${python_root}/miniforge3/envs/Stadtwetter/bin/python3" "${path_root}/auto_visualisation.py" $path_input_measured $path_input_forecast_single $path_input_forecast_multi $path_output $debug
