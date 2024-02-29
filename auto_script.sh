63a4e390db348704b720cb96ad4791ebac6a9574
Alex Steding <steding@proton.me>
2024-02-29 15:45:50 +0100

    auto_script

--- auto_script.sh
+++ auto_script.sh
@@ -1,32 +1,44 @@
 source /home/stadtwetter/.bashrc
-#conda init bash
-/home/stadtwetter/miniforge3/bin/conda activate Stadtwetter
+#conda init
 #export PYTHONPATH=$PYTHONPATH:/localdata/weathermaps/imuk
 
-path_input=/data/datenarchiv/imuk/
-path_output=/data/stadtwetter/netcdf_daten/
-#path_output=/localdata/weathermaps/webside/gross
+root_root=/home/stadtwetter #/mnt/nvmente/CODE
+#root_root=/mnt/nvmente/CODE
+
+python_root=/home/stadtwetter #/home/alex
+#python_root=/home/alex
+#"${python_root}/miniforge3/bin/conda" init zsh
+"${python_root}/miniforge3/bin/conda" activate Stadtwetter
 
 
+path_root="${root_root}/neuralcast_webside"
 
+path_input="/data/datenarchiv/imuk/"
+path_output="/data/stadtwetter/netcdf_daten/"
+
+debug=1  # true
+cd $root_root
 #convert_years(path="/data/datenarchiv/imuk/", year=2022,full=True, startday="2022-01-01",endday="2022-03-01", location="Herrenhausen", filename="test_year.nc")
 
-
-/home/stadtwetter/miniforge3/envs/Stadtwetter/bin/python3 /home/stadtwetter/neuralcast_webside/auto_convert.py $path_input $path_output
+"${python_root}/miniforge3/envs/Stadtwetter/bin/python3" "${path_root}/auto_convert.py" $path_input $path_output $debug
 echo "Converting finished"
 
+#cd $root_root
+path_input_forecast="/data/stadtwetter/netcdf_daten/"
+path_output_forecast="/data/stadtwetter/Vorhersage"
 
-path_input=/data/stadtwetter/netcdf_daten/
-path_output=/data/stadtwetter/Vorhersage/
-
-/home/stadtwetter/miniforge3/envs/Stadtwetter/bin/python3 /home/stadtwetter/neuralcast_webside/auto_forecast.py $path_input $path_output
+#
+"${python_root}/miniforge3/envs/Stadtwetter/bin/python3" "${path_root}/auto_forecast.py" $path_input_forecast $path_output_forecast $debug
 echo " Forecast finished"
 
 
 
-path_input_measured=/data/stadtwetter/netcdf_daten/
-path_input_forecast=/data/stadtwetter/Vorhersage/
-path_output=/data/stadtwetter/Vorhersage/grafiken
+path_input_measured="/data/stadtwetter/netcdf_daten/latest_herrenhausen_res_imuknet1.nc"
+path_input_forecast_single="/data/stadtwetter/Vorhersage/forecast_test_single_multiday.nc"
+path_input_forecast_multi="/data/stadtwetter/Vorhersage/forecast_test.nc"
 
-/home/stadtwetter/miniforge3/envs/Stadtwetter/bin/python3 /home/stadtwetter/neuralcast_webside/auto_visualisation.py $path_input_measured $path_input_forecast $path_output
+path_output="/data/stadtwetter/Vorhersage/grafiken/test/"
+debug=0  # true
+
+"${python_root}/miniforge3/envs/Stadtwetter/bin/python3" "${path_root}/auto_visualisation.py" $path_input_measured $path_input_forecast_single $path_input_forecast_multi $path_output $debug
 echo " Visuals finished"
